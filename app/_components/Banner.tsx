adb3833065b9f911ab4f2e9f1b7543d590c15de8
rokandic <155958535+rokandic@users.noreply.github.com>
2024-05-03 16:37:42 +0200

    Solution using React, Next.js, TypeScript, CSS modules

--- app/_components/Banner.tsx
+++ app/_components/Banner.tsx
@@ -5,17 +5,14 @@ import desktopImg from "./../_images/illustration-sign-up-desktop.svg";
 
 export default function Banner() {
   return (
-    <>
-      <picture>
-        <source media={breakpoints.mobileBreakpoint} srcSet={mobileImg.src} />
-        <source media={breakpoints.desktopBreakpoint} srcSet={desktopImg.src} />
-        <img
-          className={styles.headerImg}
-          src={mobileImg.src}
-          alt="Desktop apps with progress bar"
-        />
-      </picture>
-      <img src={mobileImg} alt="AA" />
-    </>
+    <picture>
+      <source media={breakpoints.mobileBreakpoint} srcSet={mobileImg.src} />
+      <source media={breakpoints.desktopBreakpoint} srcSet={desktopImg.src} />
+      <img
+        className={styles.headerImg}
+        src={mobileImg.src}
+        alt="Desktop apps with progress bar"
+      />
+    </picture>
   );
 }
